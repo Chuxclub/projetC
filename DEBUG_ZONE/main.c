adb9b7f36ba51b6c170f9d0516a72372ce4aa314
Florian <florian-1992@hotmail.fr>
2020-11-17 16:41:19 +0100

    Ajout d'une débug zone pour tester nos fonctions une par une...

---
+++ DEBUG_ZONE/main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+
+#include "myassert.h"
+
+#include "Voiture.h"
+#include "Collection.h"
+
+
+/*=================================================================*
+ * Programme principal
+ *=================================================================*/
+int main(int argc, char *argv[])
+{
+    
+
+    return EXIT_SUCCESS;
+}
+
